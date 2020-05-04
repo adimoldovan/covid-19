0257601a1cecf5c4d96f4653f5e59cf46ea07232
Adrian Moldovan <adim.moldovan@gmail.com>
2020-05-04 11:56:52 +0300

    Updates

---
+++ precache-manifest.7ef16252760192e2fb097ca0bac78e20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4e83d7deab1db39cc5298190d7a760e",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "5ebe20083e3136c1e7a4",
+    "url": "/covid-19/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9f04de07e5830daac834",
+    "url": "/covid-19/static/js/2.ebb0451b.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/covid-19/static/js/2.ebb0451b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ebe20083e3136c1e7a4",
+    "url": "/covid-19/static/js/main.19023425.chunk.js"
+  },
+  {
+    "revision": "f6ecbbdf84e1f735035c",
+    "url": "/covid-19/static/js/runtime-main.1ae4e48e.js"
+  }
+]);
