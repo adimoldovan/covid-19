735010ee9c2a7df9f988f223cf04c4b2693b892b
CD Pipeline <cd@trashmail.com>
2020-07-02 05:03:34 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://adimoldovan.github.io/covid-19/precache-manifest.e0760f6ab7ec499aa0b5ed2cfcfccc00.js"
+  "https://adimoldovan.github.io/covid-19/precache-manifest.13466bbea0a0e8a3e552d8e22087f248.js"
 );
 
 self.addEventListener('message', (event) => {
