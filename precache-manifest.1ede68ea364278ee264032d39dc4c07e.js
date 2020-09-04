30940e5ba9d2a020f5047c7d9f7d38f94d3a470e
CD Pipeline <cd@trashmail.com>
2020-09-04 11:12:18 +0000

    Updates

---
+++ precache-manifest.1ede68ea364278ee264032d39dc4c07e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14abc227483978b37451ec82fe489d07",
+    "url": "https://adimoldovan.github.io/covid-19/index.html"
+  },
+  {
+    "revision": "576f944d09ff08c65454",
+    "url": "https://adimoldovan.github.io/covid-19/static/css/2.fad8f5b9.chunk.css"
+  },
+  {
+    "revision": "eb81fca3f3c16402c8a4",
+    "url": "https://adimoldovan.github.io/covid-19/static/css/main.dfc49942.chunk.css"
+  },
+  {
+    "revision": "576f944d09ff08c65454",
+    "url": "https://adimoldovan.github.io/covid-19/static/js/2.d5c2194b.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "https://adimoldovan.github.io/covid-19/static/js/2.d5c2194b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb81fca3f3c16402c8a4",
+    "url": "https://adimoldovan.github.io/covid-19/static/js/main.a3bc506a.chunk.js"
+  },
+  {
+    "revision": "198aeba81d5a202e37ed",
+    "url": "https://adimoldovan.github.io/covid-19/static/js/runtime-main.557df8f9.js"
+  }
+]);
